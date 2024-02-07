e23e44aaa00dce5293f81678fc557d7f9b338337
Jonathan Johnson <me@jondavidjohn.com>
2024-02-07 10:35:04 -0600

    another commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "hc-1993a02565e34f35ac1ba9ff9b9"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
